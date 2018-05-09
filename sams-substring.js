775629d63b042d5a3cf72b65caa3721835490cf7
MSDOMAIN1\kshah1 <khyati.shah@morningstar.com>
2018-05-09 15:59:27 -0500

    minor tweaks for perf

--- sams-substring.js
+++ sams-substring.js
@@ -11,6 +11,7 @@ const getSamsSubstring = (s) => {
             moduloDictionary[i] = Math.pow(10, i);
         }
         else {
+            //(a*b) mod c = ((a mod c) * (b mod c)) mod c
             moduloDictionary[i] = (moduloDictionary[i-1] * 10) % modulo;
         }
     }
@@ -26,14 +27,16 @@ const getSamsSubstring = (s) => {
 }
 
 const sumToFallingPower = (a, n, m, modulo, moduloDictionary) => {
-    //TODO: Use modulo
     let power = n - m;
     let sum = 0;
     while (power>=0) {
         sum = sum + (moduloDictionary[power] * a);
+        if (sum >= modulo) {
+            sum %= modulo;
+        }
         power--;
     }
-    return sum * m;
+    return (sum * m) % modulo;
 }
 
 exports.getSamsSubstring = getSamsSubstring;
