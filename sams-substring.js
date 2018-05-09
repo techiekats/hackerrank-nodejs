5a3f8286543d83eeda13424f327d7ea26f77ebe7
MSDOMAIN1\kshah1 <khyati.shah@morningstar.com>
2018-05-09 15:48:03 -0500

    create modulo dict

--- sams-substring.js
+++ sams-substring.js
@@ -5,9 +5,18 @@ const getSamsSubstring = (s) => {
     let sum = 0;
     let modulo = Math.pow(10, 9) + 7;
     let multiplier = 1;
+    let moduloDictionary = {};
+    for (let i=0; i<=n; i++) {
+        if (i <= 9) {
+            moduloDictionary[i] = Math.pow(10, i);
+        }
+        else {
+            moduloDictionary[i] = (moduloDictionary[i-1] * 10) % modulo;
+        }
+    }
     for (let i=0; i<n; i++) {
         let integer = parseInt(s.charAt(i));
-        sum = sum + sumToFallingPower (integer, n, multiplier, modulo);
+        sum = sum + sumToFallingPower (integer, n, multiplier, modulo, moduloDictionary);
         multiplier++;
         if (sum >= modulo) {
             sum %= modulo;
@@ -16,12 +25,12 @@ const getSamsSubstring = (s) => {
     return sum;
 }
 
-const sumToFallingPower = (a, n, m, modulo) => {
+const sumToFallingPower = (a, n, m, modulo, moduloDictionary) => {
     //TODO: Use modulo
     let power = n - m;
     let sum = 0;
     while (power>=0) {
-        sum = sum + (Math.pow(10, power) * a);
+        sum = sum + (moduloDictionary[power] * a);
         power--;
     }
     return sum * m;
