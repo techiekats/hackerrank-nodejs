6764eb48dc309e0f86135595819d3c0f1d85daa6
kshah1 <khyati.shah@morningstar.com>
2021-12-07 00:45:13 -0600

    medians using heap sort

--- median_of_number_stream.js
+++ median_of_number_stream.js
@@ -10,8 +10,8 @@ class Heap {
         this.heap.push(num);
         let current = this.heap.length - 1;
         let parent = Math.floor((current-1)/2);
-        //assuming MaxHeap. will refactor later
-        while (this.heap[current] > this.heap[parent]) {
+        //for insert, compare current with parent. For remove, do reverse
+        while (this.compareFn(this.heap[parent], this.heap[current]) < 0) {
             [this.heap[current], this.heap[parent]] = [this.heap[parent], this.heap[current]];
             current = parent;
             parent = Math.floor((current-1)/2);
@@ -28,7 +28,7 @@ class Heap {
           let left = this.heap[current * 2 + 1];
           let right = this.heap[current * 2 + 2];
           if (!right) {
-            if (this.heap[current] < left) {
+            if (this.compareFn(this.heap[current], left) < 0) {
               [this.heap[current], this.heap[current * 2 + 1]] = [this.heap[current * 2 + 1], this.heap[current]];
               current = current * 2 + 1;
             }
@@ -36,11 +36,11 @@ class Heap {
               break;
             }
           }
-          else if (left > right && this.heap[current] < left) {            
+          else if (this.compareFn(left, right) > 0 && this.compareFn(this.heap[current], left) < 0) {            
             [this.heap[current], this.heap[current * 2 + 1]] = [this.heap[current * 2 + 1], this.heap[current]];
             current = current * 2 + 1;
           }
-          else if (right > left && this.heap[current] < right){
+          else if (this.compareFn(right, left) > 0 &&  this.compareFn(this.heap[current], right) < 0){
             [this.heap[current], this.heap[current * 2 + 2]] = [this.heap[current * 2 + 2], this.heap[current]];
             current = current * 2 + 2;
           }
@@ -53,43 +53,46 @@ class Heap {
     }
 }
 
-var h = new Heap();
+// var h = new Heap((a,b)=> b-a);
 // [25,57,48,37,12,92,86,33].forEach(x=> h.insert(x));
 // for (let i=0; i<8; i++) {
 //   console.log(h.remove());
 // }
 
-[4,5,3].forEach(x=>h.insert(x));
-for (let i=0; i<3; i++) {
-  console.log(h.remove());
+// [4,5,3].forEach(x=>h.insert(x));
+// for (let i=0; i<3; i++) {
+//   console.log(h.remove());
+//}
+function find_median_number_of_stream (arr) {
+  let minHeap = new Heap((a,b) => b-a);
+  let maxHeap = new Heap((a,b) => a-b);
+  arr.forEach(element => {
+    if (element <= maxHeap.peek() || !maxHeap.peek()) {
+      maxHeap.insert(element);
+      if (maxHeap.length() - minHeap.length() > 1) {
+        let temp = maxHeap.remove();
+        minHeap.insert(temp);
+      }    
+    }
+    else {
+      minHeap.insert(element);
+      if (minHeap.length() - maxHeap.length() > 1) {
+        let temp = minHeap.remove();
+        maxHeap.insert(temp);
+      }
+    }
+  });
+  //if both are equal in length
+  if (minHeap.length() == maxHeap.length()) {
+    return (minHeap.peek() + maxHeap.peek()) / 2;
+  }
+  if (minHeap.length() > maxHeap.length()){
+    return minHeap.peek();
+  }  
+  return maxHeap.peek();
 }
-// function find_median_number_of_stream (arr) {
-//   //contains the second half of elements
-//   let minHeap = new Heap((a,b) => a-b);
-//   //contains the first half of elements
-//   let maxHeap = new Heap((a,b) => b-a);
-//   arr.forEach(element => {
-//     if (element <= maxHeap.peek()) {
-//       maxHeap.insert(element);
-//       if (maxHeap.length() - minHeap.length() > 1) {
-//         let temp = maxHeap.remove();
-//         minHeap.insert(temp);
-//       }    
-//     }
-//     if (element >= minHeap.peek()) {
-//       minHeap.insert(element);
-//       if (minHeap.length() - maxHeap.length() > 1) {
-//         let temp = minHeap.remove();
-//         maxHeap.insert(temp);
-//       }
-//     }
-//   });
-//   //if both are even
-//   if (minHeap.length() %2 == 0 && maxHeap.length() %2 == 0) {
-//     return (minHeap.peek() + maxHeap.peek()) / 2;
-//   }
-//   if (minHeap.length() > maxHeap.length()){
-//     return minHeap.peek();
-//   }  
-//   return maxHeap.peek();
-// }
+
+console.log(find_median_number_of_stream([25,57,48,37,12,92,86,33]))
+console.log(find_median_number_of_stream([3,1]))
+console.log(find_median_number_of_stream([3,1,5]))
+console.log(find_median_number_of_stream([3,1,5,4]))
